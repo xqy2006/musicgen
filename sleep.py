7aaee9b81cdd4dc0367b34067d23d16a66c18d00
xqy2006 <48169260+xqy2006@users.noreply.github.com>
2023-06-30 21:11:02 +0800

    Create sleep.py

---
+++ sleep.py
@@ -0,0 +1,16 @@
+import requests
+import os
+import urllib3
+import time
+urllib3.disable_warnings()
+import sys
+def download(url):
+  headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:68.0) Gecko/20100101 Firefox/68.0"
+  }
+  r = requests.get(url=url, headers=headers,verify=False)
+  return r.status_code
+while download('http://xqy2006.github.io/xqy2006/musicgen/'+sys.argv[1]+'.wav')==404:
+  print("Page undeploy")
+  time.sleep(5)
+print("finish")
